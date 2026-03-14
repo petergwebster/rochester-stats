ca05bab826c09db295082514942594d637451b72
petergwebster <peter@consultexllc.com>
2026-03-14 18:52:29 -0400

    Update get-stats.js

--- netlify/functions/get-stats.js
+++ netlify/functions/get-stats.js
@@ -1,1 +1,31 @@
+const { getStore } = require("@netlify/blobs");
+
+exports.handler = async (event) => {
+  try {
+    const store = getStore("baseball-stats");
+    const data = await store.get("stats", { type: "json" });
+    if (!data) {
+      return {
+        statusCode: 404,
+        headers: { "Content-Type": "application/json", "Access-Control-Allow-Origin": "*" },
+        body: JSON.stringify({ error: "No data yet. Run scraper first." })
+      };
+    }
+    return {
+      statusCode: 200,
+      headers: {
+        "Content-Type": "application/json",
+        "Access-Control-Allow-Origin": "*",
+        "Cache-Control": "public, max-age=1800"
+      },
+      body: JSON.stringify(data)
+    };
+  } catch (err) {
+    return {
+      statusCode: 500,
+      headers: { "Content-Type": "application/json", "Access-Control-Allow-Origin": "*" },
+      body: JSON.stringify({ error: err.message })
+    };
+  }
+};
 
